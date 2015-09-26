ad8e626c95d21aa0d391b782dece2d201cba0e26
Jason Rose <jasoncrose@gmail.com>
2015-09-25 22:26:30 -0700

    Removing the old page

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ require('babel/register')
 // require React and our two React components
 var React = require('react')
 var HomePage = require('./src/home-page')
-var OtherPage = require('./src/other-page')
 
 // our hjs-webpack, of course
 var getConfig = require('hjs-webpack')
@@ -17,11 +16,9 @@ module.exports = getConfig({
     // use React's `renderToString` method to return an HTML string from our
     // components (dynamic values can be passed into `createElement` too)
     var homePageHtmlString = React.renderToString(React.createElement(HomePage))
-    var otherPageHtmlString = React.renderToString(React.createElement(OtherPage))
 
     return {
-      'index.html': data.defaultTemplate({html: homePageHtmlString}),
-      'other/index.html': data.defaultTemplate({html: otherPageHtmlString})
+      'index.html': data.defaultTemplate({html: homePageHtmlString})
 
       // here we might also create an app manifest.json
       // app-manifest.json: JSON.stringify(something)
