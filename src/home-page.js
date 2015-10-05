85224373125f385fd005e4c5bf80943ca8e3e952
Jason Rose <jasoncrose@gmail.com>
2015-10-04 19:09:58 -0700

    Breaking out parts of the homepage

--- src/home-page.js
+++ src/home-page.js
@@ -28,10 +28,136 @@ export default React.createClass({
               className="button button-outlined big-button">
           Weapons
         </Link>
+        <div className="item-group-container">
+          <Link href={`items/?weapon type=axe`}
+                className="button button-outlined big-button">
+            Axes
+          </Link>
+          <Link href={`items/?weapon type=dagger`}
+                className="button button-outlined big-button">
+            Daggers
+          </Link>
+          <Link href={`items/?weapon type=exotic`}
+                className="button button-outlined big-button">
+            Exotics
+          </Link>
+          <Link href={`items/?weapon type=flail`}
+                className="button button-outlined big-button">
+            Flails
+          </Link>
+          <Link href={`items/?weapon type=hoopak`}
+                className="button button-outlined big-button">
+            Hoopacks
+          </Link>
+          <Link href={`items/?weapon type=mace`}
+                className="button button-outlined big-button">
+            Maces
+          </Link>
+          <Link href={`items/?weapon type=polearm`}
+                className="button button-outlined big-button">
+            Polearms
+          </Link>
+          <Link href={`items/?weapon type=quarterstaff`}
+                className="button button-outlined big-button">
+            Quarterstaves
+          </Link>
+          <Link href={`items/?weapon type=spear`}
+                className="button button-outlined big-button">
+            Spears
+          </Link>
+          <Link href={`items/?weapon type=staff`}
+                className="button button-outlined big-button">
+            Staves
+          </Link>
+          <Link href={`items/?weapon type=sword`}
+                className="button button-outlined big-button">
+            Swords
+          </Link>
+          <Link href={`items/?weapon type=whip`}
+                className="button button-outlined big-button">
+            Whips
+          </Link>
+        </div>
+
         <Link href={`items/?slot=armor`}
               className="button button-outlined big-button">
           Armor
         </Link>
+        <div className="item-group-container">
+          <Link href={`items/?slot=arms`}
+                className="button button-outlined big-button">
+            Arms
+          </Link>
+          <Link href={`items/?slot=back`}
+                className="button button-outlined big-button">
+            Backs
+          </Link>
+          <Link href={`items/?slot=body`}
+                className="button button-outlined big-button">
+            Bodies
+          </Link>
+          <Link href={`items/?slot=chest`}
+                className="button button-outlined big-button">
+            Chests
+          </Link>
+          <Link href={`items/?slot=floating`}
+                className="button button-outlined big-button">
+            Floaters
+          </Link>
+          <Link href={`items/?slot=ear`}
+                className="button button-outlined big-button">
+            Ears
+          </Link>
+          <Link href={`items/?slot=face`}
+                className="button button-outlined big-button">
+            Faces
+          </Link>
+          <Link href={`items/?slot=feet`}
+                className="button button-outlined big-button">
+            Feet
+          </Link>
+          <Link href={`items/?slot=hands`}
+                className="button button-outlined big-button">
+            Hands
+          </Link>
+          <Link href={`items/?slot=head`}
+                className="button button-outlined big-button">
+            Heads
+          </Link>
+          <Link href={`items/?slot=held`}
+                className="button button-outlined big-button">
+            Held
+          </Link>
+          <Link href={`items/?slot=legs`}
+                className="button button-outlined big-button">
+            Legs
+          </Link>
+          <Link href={`items/?slot=light`}
+                className="button button-outlined big-button">
+            Lights
+          </Link>
+          <Link href={`items/?slot=neck`}
+                className="button button-outlined big-button">
+            Necks
+          </Link>
+          <Link href={`items/?slot=shield`}
+                className="button button-outlined big-button">
+            Shields
+          </Link>
+          <Link href={`items/?slot=shoulder`}
+                className="button button-outlined big-button">
+            Shoulders
+          </Link>
+          <Link href={`items/?slot=waist`}
+                className="button button-outlined big-button">
+            Waist
+          </Link>
+          <Link href={`items/?slot=wrist`}
+                className="button button-outlined big-button">
+            Wrists
+          </Link>
+        </div>
+
         <Link href={`items/?type=furniture`}
               className="button button-outlined big-button">
           Furniture
