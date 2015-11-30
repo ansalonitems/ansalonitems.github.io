5c9da766c93f1ac53daef17f31cbfd626d7cfa42
Jason Rose <jasoncrose@gmail.com>
2015-11-30 10:32:03 -0700

    First draft of optimizer

--- dobril/dobril.js
+++ dobril/dobril.js
@@ -1,12 +1,25 @@
 var downgradeEl = document.getElementById('hide-downgrades');
 if(downgradeEl) {
-    var toggleDowngradeClass = function(e) {
+    var downgradeWeaponLogic = function(bool) {
         var c = 'filter-hide-downgrades';
-        if(e.target.checked) {
+        if(bool) {
             document.body.classList.add(c);
         } else {
             document.body.classList.remove(c);
         }
     };
+    var toggleDowngradeClass = function(e) {
+        downgradeWeaponLogic(e.target.checked);
+    };
     downgradeEl.addEventListener('click', toggleDowngradeClass);
+    toggleDowngradeClass({target: downgradeEl});
+}
+
+var optimizerEl = document.getElementById('optimizer-submit');
+if(optimizerEl) {
+    var goToOptimizedPage = function() {
+        var level = document.getElementById('optimizer-level').value;
+        document.location = '/dobril/optimizer/' + (level) + '.html'
+    };
+    optimizerEl.addEventListener('click', goToOptimizedPage);
 }
